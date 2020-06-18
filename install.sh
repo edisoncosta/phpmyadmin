defb6761b78d518d22bc14bb6b2e89080b8507eb
Edison Costa <edison@agiuscloud.com>
2020-06-18 18:06:26 -0300

    chown phpmyadmin

--- install.sh
+++ install.sh
@@ -9,3 +9,4 @@ wget https://files.phpmyadmin.net/phpMyAdmin/$VERSION/phpMyAdmin-$VERSION-all-la
 tar xzvf phpMyAdmin-$VERSION-all-languages.tar.gz 
 mv phpMyAdmin-$VERSION-all-languages/* /var/www/tools/phpmyadmin/htdocs
 cp /var/www/tools/phpmyadmin/htdocs/config.sample.inc.php /var/www/tools/phpmyadmin/htdocs/config.inc.php
+chown -R phpmyadmin:phpmyadmin /var/www/tools/phpmyadmin/htdocs
